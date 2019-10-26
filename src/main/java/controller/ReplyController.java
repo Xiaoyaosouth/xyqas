01df6d4eff4200ac2c89051e391ac0a123663666
xiaoyao <626753724@qq.com>
2019-10-27 00:12:40 +0800

    回贴后,刷新贴子的更新时间、回复数

--- src/main/java/controller/ReplyController.java
+++ src/main/java/controller/ReplyController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import service.ReplyService;
+import service.TipService;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -20,27 +21,36 @@ public class ReplyController {
     @Resource
     private ReplyService replyService;
 
+    @Resource
+    private TipService tipService;
+
     @Autowired
     private HttpServletRequest request;
 
     @Autowired
     private HttpSession session;
 
+    /**
+     * 回贴控制
+     * @param reply 回复实体对象
+     * @return
+     */
     @RequestMapping("publishReply.do")
     public ModelAndView addReply(Reply reply){
         ModelAndView mv = new ModelAndView();
         // 处理参数
         int userid = Integer.valueOf(request.getParameter("user_id"));
         int tipid = Integer.valueOf(request.getParameter("tip_id"));
-        System.out.println("从前台获取的回复用户id为：" + userid);
-        System.out.println("从前台获取的贴子id为：" + tipid);
-        System.out.println("从前台获取的回复内容为：" + reply.getReply_content());
         reply.setUser_id(userid);
         reply.setTip_id(tipid);
-        // 初始化回贴时间
+        // 回复产生时间
         Date date = new Date();
         reply.setReply_publishTime(date);
         String resultStr = replyService.addReply(reply);
+        // 刷新贴子更新时间
+        tipService.updateModifyTimeByTipId(tipid, date);
+        // 更新贴子回复数
+        tipService.updateRepliesByTipId(tipid);
         request.setAttribute("myInfo", resultStr);
         mv.setViewName("redirect:showTip.do?tipId=" + tipid);
         return mv;

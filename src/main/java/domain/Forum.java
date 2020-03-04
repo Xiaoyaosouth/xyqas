89c3e34f9cf02870948df6d3e45c71d22fa3b7f5
xiaoyao <626753724@qq.com>
2020-03-04 11:57:10 +0800

    增加创建时间、更新时间属性

--- src/main/java/domain/Forum.java
+++ src/main/java/domain/Forum.java
@@ -2,9 +2,12 @@ package domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 版块实体类
  * v1.1 2020-02-28 10:18 删除版块状态属性，增加是否删除属性
+ * v1.2 2020-03-04 11:54 增加创建时间、修改时间属性
  */
 @Data
 public class Forum {
@@ -13,5 +16,7 @@ public class Forum {
     private String forum_name; // 版块名
     // private int forum_status; // 版块状态
     private int forum_isDeleted; // 是否删除，0-否，1-是
+    private Date forum_createTime; // 创建时间
+    private Date forum_modifyTime; // 修改时间
 
 }
--- src/main/java/domain/Tab.java
+++ src/main/java/domain/Tab.java
@@ -2,9 +2,12 @@ package domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 分类实体类
  * v1.1 2020-02-28 10:17 删除分类状态属性，增加是否删除属性
+ * v1.2 2020-03-04 10:51 增加创建时间、修改时间属性
  */
 @Data
 public class Tab {
@@ -16,5 +19,7 @@ public class Tab {
     private String tab_name; // 分类名
     // private int tab_status; // 分类状态
     private int tab_isDeleted; // 是否删除，0-否，1-是
+    private Date tab_createTime; // 创建时间
+    private Date tab_modifyTime; // 修改时间
 
 }
